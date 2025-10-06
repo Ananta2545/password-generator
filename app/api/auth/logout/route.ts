bef5d5754a459d7c471bb2d1e488edb52435f4df
Ananta2545 <chatterjeeanata091@gmail.com>
2025-10-07 03:06:56 +0530

    added logout route

---
+++ app/api/auth/logout/route.ts
@@ -0,0 +1,53 @@
+import { verifyToken } from "@/app/lib/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest){
+    try{
+        const token = req.cookies.get('token')?.value || req.headers.get('authorization')?.replace('Bearer ', '');
+
+        if(!token){
+            return NextResponse.json(
+                {success: false, message: "No active session found"},
+                {status: 400},
+            );
+        }
+
+        const decoded = verifyToken(token);
+        if(!decoded){
+            return NextResponse.json(
+                {success: false, message: "Invalid session"},
+                {status: 401},
+            );
+        }
+
+        const response = NextResponse.json(
+            {
+                success: true,
+                message: "Logged out successfully",
+            },
+            {
+                status: 200
+            }
+        );
+
+        response.cookies.set('token', '', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            path: '/',
+            maxAge: 0,
+        });
+
+        return response;
+
+    }catch(error){
+        let message = "Failed to logout";
+        if(error instanceof Error){
+            message = error.message;
+        }
+        return NextResponse.json(
+            {success: false, message, error: message},
+            {status: 500}
+        );
+    }
+}
